8d8271456a1c85359f2926fa1f3213e156afce2d
Hargun-singhh <66933848+Hargun-singhh@users.noreply.github.com>
2020-06-27 12:15:32 +0530

    covid19

---
+++ MIni Project.py
@@ -0,0 +1,151 @@
+print("ORDER FOOD ONLINE FROM ZOMATO")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("TIKKA JUNCTION")
+print("~~~~~~~~~~~~~~")
+print("TIME TO DELIVER = 40 MINS")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~")
+products = {
+    "kadhai paneer": 340,
+    "dal makhani": 320,
+    "soya chaap casala": 340,
+    "veg soya chaap": 390,
+    "afghani chaap tikka ": 330,
+    "veg tandoori momos": 280,
+    "better naan": 65,
+    "tandoori roti": 30,
+    "paneer tikka roll": 290,
+    "romali roti": 35,
+    "butter roti": 35,
+    "atta lachha paratha": 55,
+    "mineral water ": 49,
+    "veg hot and sour soup": 140,
+    "veg lemon coriander": 140,
+    "veg manchow soup": 140,
+    "veg sweet corn soup": 140,
+    "veg talumein soup": 140,
+    "mushroom malai tikka": 330,
+    "mashroom tikka": 330,
+    "paneer achari tikka": 330,
+    "paneer kali mirch tikka": 330,
+    "paneer lahsuni tikka": 330,
+    "paneer malai tikka": 330,
+    "paneer tikka": 330,
+    "peri peri chaap tikka": 330,
+    "paneer papdi": 330,
+    "dahi ke sholey": 330,
+    "dahi kebab": 330,
+    "french fries": 220,
+    "chilli mushroom": 330,
+    "chilli paneer": 330,
+    "crispy chilli potatoes": 310,
+    "crispy honey chilli soya": 310,
+    "honey chilli potatoes": 310,
+    "manchurian dry": 330,
+    "soya drums of heaven": 330,
+    "spring roll": 310,
+    "tandoori spring roll": 310,
+    "soya schewan": 330,
+    "crispy veg salt and pepper": 310,
+    "pepricano roll": 310,
+    "veg satay": 310,
+    "dal dhaba": 320,
+    "paneer  butter masala": 340,
+    "paneer lababdar": 340,
+    "shahi paneer ": 330,
+    "paneer bhurji": 330,
+    "moong dal tadka": 320,
+    "mix veg": 340,
+    "palak paneer ": 340,
+    "paneer methi malai": 340,
+    "malai kofta": 370,
+    "chilly mushroom gravy": 350,
+    "chilly paneer gravy": 350,
+    "veg manchurian gravy": 350,
+    "mixed veg in hot garlic sauce": 350,
+    "plain rice": 200,
+    "jeera rice": 230,
+    "veg biryani": 320,
+    "streamed rice": 190,
+    "veg  chilli garlic rice ": 230,
+    "veg hakka noodles": 230,
+    "veg butter noodles": 250,
+    "veg pan fried noodles": 250,
+    "veg chopsuey": 290,
+    "mushroom tikka roll": 290,
+    "soya roll": 290,
+    "veg singapore noodles": 340,
+    "paneer tikka paratha roll": 230,
+    "kurkure veg momos": 280,
+    "veg fried momos": 260,
+    "veg honey chilli toss momos": 280,
+    "veg streamed momos": 260,
+    "cream roti": 40,
+    "masala roti": 65,
+    "gralic roti": 45,
+    "naan": 55,
+    "masala naan": 65,
+    "butter naan": 65,
+    "mirchi paratha": 65,
+    "garlic naan": 75,
+    "paneer naan": 85,
+    "paneer paratha": 75,
+    "blue lagoon": 199,
+    "virjin mojito": 199,
+    "kiwi blast": 199,
+    "red bull": 199,
+    "masala papad": 140,
+    "peanut papad": 200,
+
+}
+
+cart = []
+choice = "yes"
+
+quantity = 1
+
+while choice == "yes":
+    product = input("Enter Dish of Your Choice:")
+    quantity = int(input("Enter QUANTITY OF Dish:"))
+    cart.append(products[product])
+    choice = input("Would you like to add another dish (yes/no)")
+
+print("Your Cart [", len(cart), "]:")
+print(cart)
+
+
+total = 0
+
+delivery = 50
+
+for price in cart:
+    total = total + price * quantity
+
+print("TOTAL:", total)
+
+if total >= 500:
+    promoCode = input("Enter the Promo Code: ")
+
+    if promoCode == "EATREPEAT":
+        discount = 0.40 * total
+
+        if discount >= 200:
+            discount = 200
+
+        amountToPay = total - discount + delivery
+        print("EATREPEAT Applied")
+        print("Discount of \u20b9", discount, "applied. Please pay total including delivery charges : \u20b9", amountToPay)
+
+    elif promoCode == "TRYNOW":
+        discount = 0.50 * total
+
+        if discount >= 400:
+            discount = 400
+
+        amountToPay = total - discount + delivery
+
+        print("TRYNOW Applied")
+        print("Discount of \u20b9", discount, "Applied. Please Pay Total Incluing Delivery Charges: \u20b9", amountToPay)
+
+    else:
+        print("No PromoCode Applicable")
+        print("Please Pay: \u20b9", total)
--- new covid19 cases.py
+++ new covid19 cases.py
@@ -1,225 +1,226 @@
 state1 = {
     "active": 881,
-	"confirmed": 3497,
-	"deaths": 78,
-	"recovered": 2538,
-	"state": "Punjab"
+    "confirmed": 3497,
+    "deaths": 78,
+    "recovered": 2538,
+    "state": "Punjab"
 
 }
 state2 = {
     "active": 51922,
-	"confirmed": "116752",
-	"deaths": 5651,
-	"recovered": 59166,
-	"state": "Maharashtra"
+    "confirmed": "116752",
+    "deaths": 5651,
+    "recovered": 59166,
+    "state": "Maharashtra"
 
 }
 state3 = {
-	"active": 21993,
-	"confirmed": 50193,
-	"deaths": 576,
-	"recovered": 27624,
-	"state": "Tamil Nadu"
+    "active": 21993,
+    "confirmed": 50193,
+    "deaths": 576,
+    "recovered": 27624,
+    "state": "Tamil Nadu"
 
 }
 state4 = {
-	"active": 27741,
-	"confirmed": 47102,
-	"deaths": 1904,
-	"recovered": 17457,
-	"state": "Delhi"
+    "active": 27741,
+    "confirmed": 47102,
+    "deaths": 1904,
+    "recovered": 17457,
+    "state": "Delhi"
 
 }
 state5 = {
-	"active": 6149,
-	"confirmed": 25148,
-	"deaths": "1561",
-	"recovered": 17438,
-	"state": "Gujarat"
+    "active": 6149,
+    "confirmed": 25148,
+    "deaths": "1561",
+    "recovered": 17438,
+    "state": "Gujarat"
 
 }
 state6 = {
-	"active": "162534",
-	"confirmed": "372552",
-	"deaths": "12390",
-	"recovered": "197583",
-	"state": "Total"
-
+    "active": "162534",
+    "confirmed": "372552",
+    "deaths": "12390",
+    "recovered": "197583",
+    "state": "Total"
 
 }
 state7 = {
-	"active": "5659",
-	"confirmed": "15785",
-	"deaths": "488",
-	"recovered": "9638",
-	"state": "Uttar Pradesh"
+    "active": "5659",
+    "confirmed": "15785",
+    "deaths": "488",
+    "recovered": "9638",
+    "state": "Uttar Pradesh"
 
 }
 state8 = {
-	"active": "2721",
-	"confirmed": "13626",
-	"deaths": "323",
-	"recovered": "10582",
-	"state": "Rajasthan"
+    "active": "2721",
+    "confirmed": "13626",
+    "deaths": "323",
+    "recovered": "10582",
+    "state": "Rajasthan"
 
 }
 state9 = {
-	"active": "5216",
-	"confirmed": "12735",
-	"deaths": "518",
-	"recovered": "7001",
-	"state": "West Bengal"
+    "active": "5216",
+    "confirmed": "12735",
+    "deaths": "518",
+    "recovered": "7001",
+    "state": "West Bengal"
 
 }
 state10 = {
-	"active": "2308",
-	"confirmed": "11426",
-	"deaths": "486",
-	"recovered": "8632",
-	"state": "Madhya Pradesh"
+    "active": "2308",
+    "confirmed": "11426",
+    "deaths": "486",
+    "recovered": "8632",
+    "state": "Madhya Pradesh"
 
 }
 state11 = {
-	"active": "4710",
-	"confirmed": "9743",
-	"deaths": "144",
-	"recovered": "4889",
-	"state": "Haryana"
+    "active": "4710",
+    "confirmed": "9743",
+    "deaths": "144",
+    "recovered": "4889",
+    "state": "Haryana"
 }
 state12 = {
-	"active": "2939",
-	"confirmed": "8281",
-	"deaths": "125",
-	"recovered": "5213",
-	"state": "Karnataka"
+    "active": "2939",
+    "confirmed": "8281",
+    "deaths": "125",
+    "recovered": "5213",
+    "state": "Karnataka"
 }
-state13 ={
-	"active": "4240",
-	"confirmed": "8452",
-	"deaths": "101",
-	"recovered": "4111",
-	"state": "Andhra Pradesh"
+state13 = {
+    "active": "4240",
+    "confirmed": "8452",
+    "deaths": "101",
+    "recovered": "4111",
+    "state": "Andhra Pradesh"
 }
-state14 ={
-	"active": "2087",
-	"confirmed": "7503",
-	"deaths": "49",
-	"recovered": "5367",
-	"state": "Bihar"
+state14 = {
+    "active": "2087",
+    "confirmed": "7503",
+    "deaths": "49",
+    "recovered": "5367",
+    "state": "Bihar"
 }
-state15 ={
-	"active": "3363",
-	"confirmed": "7072",
-	"deaths": "203",
-	"recovered": "3506",
-	"state": "Telangana"
+state15 = {
+    "active": "3363",
+    "confirmed": "7072",
+    "deaths": "203",
+    "recovered": "3506",
+    "state": "Telangana"
 }
 state16 = {
-	"active": "2417",
-	"confirmed": "5834",
-	"deaths": "81",
-	"recovered": "3336",
-	"state": "Jammu and Kashmir"
+    "active": "2417",
+    "confirmed": "5834",
+    "deaths": "81",
+    "recovered": "3336",
+    "state": "Jammu and Kashmir"
 }
 state17 = {
-	"active": "1791",
-	"confirmed": "5006",
-	"deaths": "9",
-	"recovered": "3203",
-	"state": "Assam"
+    "active": "1791",
+    "confirmed": "5006",
+    "deaths": "9",
+    "recovered": "3203",
+    "state": "Assam"
 }
 state18 = {
-	"active": "1307",
-	"confirmed": "4856",
-	"deaths": "15",
-	"recovered": "3534",
-	"state": "Odisha"
+    "active": "1307",
+    "confirmed": "4856",
+    "deaths": "15",
+    "recovered": "3534",
+    "state": "Odisha"
 }
 state19 = {
-	"active": "1450",
-	"confirmed": "3040",
-	"deaths": "22",
-	"recovered": "1566",
-	"state": "Kerala"
+    "active": "1450",
+    "confirmed": "3040",
+    "deaths": "22",
+    "recovered": "1566",
+    "state": "Kerala"
 }
 state20 = {
-	"active": "809",
-	"confirmed": "2301",
-	"deaths": "27",
-	"recovered": "1450",
-	"state": "Uttarakhand"
+    "active": "809",
+    "confirmed": "2301",
+    "deaths": "27",
+    "recovered": "1450",
+    "state": "Uttarakhand"
 }
 state21 = {
-	"active": "755",
-	"confirmed": "2134",
-	"deaths": "11",
-	"recovered": "1368",
-	"state": "Chhattisgarh"
+    "active": "755",
+    "confirmed": "2134",
+    "deaths": "11",
+    "recovered": "1368",
+    "state": "Chhattisgarh"
 }
 state22 = {
-	"active": "615",
-	"confirmed": "1961",
-	"deaths": "11",
-	"recovered": "1335",
-	"state": "Jharkhand"
+    "active": "615",
+    "confirmed": "1961",
+    "deaths": "11",
+    "recovered": "1335",
+    "state": "Jharkhand"
 }
 state23 = {
-	"active": "531",
-	"confirmed": "1189",
-	"deaths": "1",
-	"recovered": "657",
-	"state": "Tripura"
+    "active": "531",
+    "confirmed": "1189",
+    "deaths": "1",
+    "recovered": "657",
+    "state": "Tripura"
 }
 state24 = {
-	"active": "718",
-	"confirmed": "836",
-	"deaths": "1",
-	"recovered": "117",
-	"state": "Ladakh"
+    "active": "718",
+    "confirmed": "836",
+    "deaths": "1",
+    "recovered": "117",
+    "state": "Ladakh"
 }
 state25 = {
-	"active": "625",
-	"confirmed": "754",
-	"deaths": "0",
-	"recovered": "129",
-	"state": "Goa"
+    "active": "625",
+    "confirmed": "754",
+    "deaths": "0",
+    "recovered": "129",
+    "state": "Goa"
 }
 state26 = {
-	"active": "228",
-	"confirmed": "650",
-	"deaths": "7",
-	"recovered": "402",
-	"state": "Himachal Pradesh"
+    "active": "228",
+    "confirmed": "650",
+    "deaths": "7",
+    "recovered": "402",
+    "state": "Himachal Pradesh"
 }
 state27 = {
-	"active": "463",
-	"confirmed": "681",
-	"deaths": "0",
-	"recovered": "218",
-	"state": "Manipur"
+    "active": "463",
+    "confirmed": "681",
+    "deaths": "0",
+    "recovered": "218",
+    "state": "Manipur"
 }
 state28 = {
-	"active": "68",
-	"confirmed": "390",
-	"deaths": "6",
-	"recovered": "316",
-	"state": "Chandigarh"
+    "active": "68",
+    "confirmed": "390",
+    "deaths": "6",
+    "recovered": "316",
+    "state": "Chandigarh"
 }
 state29 = {
-	"active": "121",
-	"confirmed": "135",
-	"deaths": "0",
-	"recovered": "14",
-	"state": "Arunachal Pradesh"
+    "active": "121",
+    "confirmed": "135",
+    "deaths": "0",
+    "recovered": "14",
+    "state": "Arunachal Pradesh"
 }
 state30 = {
-	"active": "200",
-	"confirmed": "338",
-	"deaths": "7",
-	"recovered": "131",
-	"state": "Puducherry"
+    "active": "200",
+    "confirmed": "338",
+    "deaths": "7",
+    "recovered": "131",
+    "state": "Puducherry"
 }
-india = [state2, state3, state4, state5, state1, state7, state8, state9, state10, state11, state12, state13, state14, state15, state16, state17, state18, state19, state20, state21, state22, state23, state24, state25, state26, state27, state28, state29, state30, state6]
+india = [state2, state3, state4, state5, state1, state7, state8, state9, state10, state11, state12, state13, state14,
+         state15, state16, state17, state18, state19, state20, state21, state22, state23, state24, state25, state26,
+         state27, state28, state29, state30, state6]
 print("Length of India is:", len(india))
 print()
 
@@ -228,18 +229,19 @@ filter2 = input("Please Enter 2nd Filter from [active | confirmed | deaths | rec
 filter3 = input("Please Enter 2nd Filter from [active | confirmed | deaths | recovered | state]: ")
 
 for i in range(0, len(india)):
-	print("~~~~~~~~~~~~~~~~~~")
-	print("{}: {}\n{}: {}\n{}: {}".format(filter1.upper(), india[i][filter1], filter2.upper(), india[i][filter2], filter3.upper(), india[i][filter3]))
-	print("~~~~~~~~~~~~~~~~~~")
-	print()
+    print("~~~~~~~~~~~~~~~~~~")
+    print("{}: {}\n{}: {}\n{}: {}".format(filter1.upper(), india[i][filter1], filter2.upper(), india[i][filter2],
+                                          filter3.upper(), india[i][filter3]))
+    print("~~~~~~~~~~~~~~~~~~")
+    print()
 
 state = input("Enter the name of state to search: ")
 for i in range(0, len(india)):
 
-	print("Matching {} with {}".format(state, india[i]["state"]))
+    print("Matching {} with {}".format(state, india[i]["state"]))
 
-	# if state == india[i]["state"]:
-	if state.lower() == india[i]["state"].lower():
-		print("State {} Found. Details are Below:".format(state))
-		print(india[i])
-		break
+    # if state == india[i]["state"]:
+    if state.lower() == india[i]["state"].lower():
+        print("State {} Found. Details are Below:".format(state))
+        print(india[i])
+        break
